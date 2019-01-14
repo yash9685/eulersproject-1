90d73bf45e7f35756d0f18e67e11dea0de2bf86a
swetasahoo29 <46675936+swetasahoo29@users.noreply.github.com>
2019-01-14 21:07:17 +0530

    Add files via upload

---
+++ euler4.java
@@ -0,0 +1,38 @@
+
+public class euler4 {
+
+	public static void main(String[] args) {
+		int prd=1;
+		int maxpal= -1;
+		for(int i=999;i>=100;i--)
+		{
+			for(int j=999;j>=100;j--)
+			{
+				prd=i*j;
+				int temp=prd;
+				if(ispal(prd) && prd>maxpal)
+					maxpal=temp;
+			}
+		}
+		System.out.println(maxpal);
+	}
+	static boolean ispal(int n){
+	
+		int r=0;
+		int x;
+		int temp=n;
+		while(n>0)
+		{
+			x=n%10;
+			r=r*10+x;
+			n/=10;
+			}
+		return temp==r;
+		}
+		
+			}
+		
+
+	
+
+}
