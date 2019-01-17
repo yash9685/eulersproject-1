4d630e8c9f4b5cd341b4fedfe529ef2a059675bb
swetasahoo29 <46675936+swetasahoo29@users.noreply.github.com>
2019-01-17 20:38:04 +0530

    Add files via upload

--- euler5.java
+++ euler5.java
@@ -2,25 +2,27 @@
 public class euler5 {
 
 	public static void main(String[] args) {
-		int s=0;
-		long i;
-		System.out.println("programme initiated...");
-		for(i=2521;s!=19;i++)
-		{
-			for(int j=2;j<=20;j++)
-			{
-				if(i%j==0)
-					s++;
-				else
-				{
-					s=0;
-					break;
-				}
-			}
-		}
-		System.out.println("smallest divisible number between 1 to 20 is"+(i-1));
+		long num=20;
+		System.out.println(lcm(num));
 	}
-}
+	public static long lcm(long num)
+	{
+		long l=1;
+		for(long i=1;i<=num;i++)
+		
+			l=l*i/(gcd(l,i));
+			return l;
+			
+		}
+	public static long gcd(long l,long i)
+	{
+		if(l%i!=0)
+			return gcd(i,l%i);
+			else
+				return i;
+		}
+	}
+
 				
 			
 	
