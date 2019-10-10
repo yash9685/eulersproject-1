e6c44e792c55a1549102733eb62a8c2ac66518b7
Adarsh Padhi <45957713+adarsh1405@users.noreply.github.com>
2019-10-10 15:03:46 +0530

    optimize eluerQ6

--- euler6.java
+++ euler6.java
@@ -1,21 +1,11 @@
 
-public class euler6 {
-
-	public static void main(String[] args) {
-	int s=0;
-	int prd=0;
-	for(int i=1;i<=100;i++)
-	{
-		s=s+i;
-		prd=prd + (i*1);
-	}
-	s=s*s;
-	int difference=s-prd;
-	System.out.println(difference);
-	}
+class euler6
+{
+public static void main(String[] args) {
+	int sumsq,sqsum,diff,n=100;
+	sumsq=(n*(n+1))/2;
+	sqsum=(n*(n+1)*((2*n)+1))/6;
+	diff=(sumsq*sumsq)-sqsum;
+	System.out.println(diff);	
 }
-	
-
-	
-
-
+}
